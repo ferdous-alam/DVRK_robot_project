55de338ee1f38bd568714161e1336514ed51cc27
mechyai <65429130+mechyai@users.noreply.github.com>
2021-12-09 11:16:09 -0500

    Update test_dvrk.py

--- Codes/test_dvrk.py
+++ Codes/test_dvrk.py
@@ -1,12 +1,28 @@
 import numpy as np
+import rospy
+
+import tf_conversions.posemath as pm
+import PyKDL
 
 
 def test_dvrk(p):
     tau = np.load('tau.npy')
-    start = p.measured_cp()
-    for i in range(len(tau)):
-        start.p[0] = tau[i][0, 3]
-        start.p[1] = tau[i][1, 3]
-        start.p[2] = tau[i][2, 3] - 0.060
+    start = p.setpoint_cp()
+    mcp = []
+    for numpy_transformation in tau:
+        kdl_transformation = pm.fromMatrix(numpy_transformation)  # convert from np to kdl transformation
+	pos = kdl_transformation.p  # extract position vector
+	rot = kdl_transformation.M  # extract rotation matrix
+	print(rot,pos)
+	# communicate to robot	
+	start.p = pos
+	start.M = rot
+        p.move_cp(start)
 
-        p.move_cp(start).wait()
+
+	#cp_val = p.measured_cp()	
+	#print(cp_val)
+	#mcp.append(p.measured_cp())	
+	# rate.sleep()
+
+    #np.save('mcp.npy', mcp)
